c62eb3abc8331ec9f8ca595f375b6942f8b1ba1c
dthv201 <gaya.vishna@gmail.com>
2025-07-14 19:26:19 +0300

    Perfect swiching between the cameras and animetion is ok

--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
 using Unity.Cinemachine;
 
+
 public class CameraSwitcher : MonoBehaviour
 {
     [Header("Cinemachine Cameras")]
-    public CinemachineFreeLook thirdPersonCam;   // your existing 3rd-person FreeLook
-    public CinemachineVirtualCamera firstPersonCam; // new FPS virtual camera
+    public CinemachineFreeLook thirdPersonCam;     // your FreeLook rig
+    public CinemachineVirtualCamera firstPersonCam; // your FPS rig
 
     [Header("Key to Switch")]
     public KeyCode switchKey = KeyCode.V;
 
+    // Name of the layer you created for your player mesh
+    const string PlayerBodyLayerName = "PlayerBody";
+
     private bool usingFirstPerson = false;
+    private Camera mainCam;
+    private int playerBodyLayerMask;
 
     void Start()
     {
-        // start in third-person
+        // Cache references
+        mainCam = Camera.main;
         thirdPersonCam.gameObject.SetActive(true);
         firstPersonCam.gameObject.SetActive(false);
+
+        // Build a bitmask for your PlayerBody layer
+        int layerIndex = LayerMask.NameToLayer(PlayerBodyLayerName);
+        playerBodyLayerMask = 1 << layerIndex;
     }
 
     void Update()
@@ -28,7 +39,21 @@ public class CameraSwitcher : MonoBehaviour
     void ToggleCameras()
     {
         usingFirstPerson = !usingFirstPerson;
+
+        // Switch the rigs
         thirdPersonCam.gameObject.SetActive(!usingFirstPerson);
         firstPersonCam.gameObject.SetActive(usingFirstPerson);
+
+        // Modify culling mask so PlayerBody is only visible in 3rd-person
+        if (usingFirstPerson)
+        {
+            // remove PlayerBody bit
+            mainCam.cullingMask &= ~playerBodyLayerMask;
+        }
+        else
+        {
+            // add PlayerBody bit back
+            mainCam.cullingMask |= playerBodyLayerMask;
+        }
     }
 }
